d0cd828ac28e791ec10ff468a64ea7d2b51f5d88
JahongirFox <raxmatjonovjahongir5@gmail.com>
2025-08-14 13:19:42 +0500

    BUGS FIXED

--- spiral.py
+++ spiral.py
@@ -1,14 +1,12 @@
 import turtle
 import colorsys
 
-# Ekranni sozlash
 t = turtle.Turtle()
 s = turtle.Screen()
 s.bgcolor("black")
 t.speed(0)
 turtle.colormode(255)
 
-# Ranglar ro'yxati
 n = 36
 h = 0
 colors = []
@@ -17,12 +15,11 @@ for i in range(n):
     colors.append((int(col[0]*255), int(col[1]*255), int(col[2]*255)))
     h += 1/n
 
-# Spiral naqsh chizish
 t.width(2)
 for i in range(360):
     t.color(colors[i % n])
     t.forward(i * 2)
-    t.right(59)  # burchak o'zgartirish spirallikni beradi
+    t.right(59) 
 
 t.hideturtle()
 turtle.done()
