94499c702c679a3a20ebf0f53c4e52a06a120e9e
JahongirFox <raxmatjonovjahongir5@gmail.com>
2025-08-14 13:21:03 +0500

    BUGS FIXED

--- trees.py
+++ trees.py
@@ -1,20 +1,18 @@
 import turtle
 
-# Ekranni sozlash
 screen = turtle.Screen()
 screen.bgcolor("black")
 t = turtle.Turtle()
 t.color("lime")
 t.speed(0)
 t.hideturtle()
-t.left(90)  # yuqoriga qarab boshlaydi
+t.left(90) 
 t.penup()
 t.goto(0, -250)
 t.pendown()
 
-# Fraktal daraxt funksiyasi
 def draw_tree(branch_length):
-    if branch_length < 10:  # eng kichik shox
+    if branch_length < 10: 
         return
     t.forward(branch_length)
     t.left(30)
@@ -24,7 +22,6 @@ def draw_tree(branch_length):
     t.left(30)
     t.backward(branch_length)
 
-# Daraxtni chizish
 draw_tree(100)
 
 turtle.done()
